4f3388479e27a3c9bf5f821be635459fa55c44aa
Broccoli <661501068@lamduan.mfu.ac.th>
2025-08-27 02:58:35 +0700

    main

--- bin/projectkub_2.dart
+++ bin/projectkub_2.dart
@@ -1,132 +1,81 @@
+// for http connection
 import 'package:http/http.dart' as http;
+// for stdin
 import 'dart:io';
-import 'dart:convert';
-
 
 void main() async {
-  print("===Login===");
+  await login();
+}
 
-
+Future<void> login() async {
+  print("===== Login =====");
+  // Get username and password
   stdout.write("Username: ");
   String? username = stdin.readLineSync()?.trim();
-  if (username == null || username.isEmpty) {
-    print("please put your name");
-    return;
-  }
-
-
   stdout.write("Password: ");
   String? password = stdin.readLineSync()?.trim();
-  if (password == null || password.isEmpty) {
-    print("please put your password");
+  if (username == null || password == null) {
+    print("Incomplete input");
     return;
   }
 
+  final body = {"username": username, "password": password};
+  final url = Uri.parse('http://localhost:3000/login');
+  final response = await http.post(url, body: body);
+  // note: if body is Map, it is encoded by "application/x-www-form-urlencoded" not JSON
+  if (response.statusCode == 200) {
+    // the response.body is String
+    final result = response.body;
+    Choose(username);
+    // เข้า Function Here
+  } else if (response.statusCode == 401 || response.statusCode == 500) {
+    final result = response.body;
+    print(result);
+  } else {
+    print("Unknown error");
+  }
+}
 
-  // ===== LOGIN =====
-  var loginRes = await http.post(
-    Uri.parse('http://localhost:3000/login'),
-    headers: {"Content-Type": "application/json"},
-    body: jsonEncode({'username': username, 'password': password}),
-  );
-
-
-  print("Insert done");
-
-
-  var loginData = jsonDecode(loginRes.body);
-
-
-  int userId = loginData['user_id'];
-
-
-  // ===== MENU =====
+Future<void> Choose(name) async {
   while (true) {
-    print("\n===Expenses Tracking App===");
+    print("============ Expense Tracking App ============");
+    print("WElcome $name");
     print(
-      "1. Show All Expenses"
-      "\n2. Today's Expenses"
-      "\n3. Search Expenses"
-      "\n4. Add new Expenses"
-      "\n5. Delete an Expenses"
-      "\n6. Exit",
+      " 1. All expenses \n 2. Today's expense \n 3. Search expense \n 4. Add new expense \n 5. Delete an expense \n 6. Exit",
     );
 
+    stdout.write("Choose .. ");
+    final input = stdin.readLineSync();
+    final choice = int.tryParse(input ?? "");
 
-    stdout.write("Choose... ");
-    String? choice = stdin.readLineSync()?.trim();
-    if (choice == null || choice.isEmpty) {
-      print("please choose a number");
-      return;
-    }
-
-    //===Choice 1===
- 
-    
-    //===Choice 2===
-
-    
-    
-    ////===Choice 3=== Search Expenses ====
-   
-   
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-   //===Choice 4=== Add new Expenses ====
-    
-  
-      
-    
-    
-
-
-
-
-
-  
-
-
-
-
-
-  
-    //===Choice 5=== Delete an Expenses ====
-    
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-    //===Choice 6===
-    else if (choice == "6") {
-      print("Good Bye");
-      break;
+    if (choice == 1) {
+      ShowallEXpense();
+    } else if (choice == 2) {
+      ShowallEXpenseToday();
+    } else if (choice == 3) {
+      SearchExpense();
+    } else if (choice == 4) {
+      AddNewExpense();
+    } else if (choice == 5) {
+      DeleteAnExpense();
+    } else {
+      print("--- bye ---");
     }
   }
+}
+
+Future<void> ShowallEXpense() async {
+
+}
+Future<void> ShowallEXpenseToday() async {
+
+}
+Future<void> SearchExpense() async {
+
+}
+Future<void> AddNewExpense() async {
+
+}
+Future<void> DeleteAnExpense() async {
+  
 }
