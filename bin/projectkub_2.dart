b3c4c72fb1f4ed9197bd674a1da7c73ab1966099
Broccoli <661501068@lamduan.mfu.ac.th>
2025-08-27 23:55:24 +0700

    function show all done

--- bin/projectkub_2.dart
+++ bin/projectkub_2.dart
@@ -1,4 +1,4 @@
-// for http connection
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 // for stdin
 import 'dart:io';
@@ -63,9 +63,18 @@ Future<void> Choose(name) async {
     }
   }
 }
-
+final baseUrl = "http://localhost:3000/";
 Future<void> ShowallEXpense() async {
-
+  final res = await http.get(Uri.parse("$baseUrl/Expense"));
+  if (res.statusCode == 200) {
+    final data = jsonDecode(res.body);
+    print("===== All Expenses =====");
+    for (var e in data) {
+      print("${e['id']}. ${e['item']} - ${e['paid']} THB [${e['date']}]");
+    }
+  } else {
+    print("Error: ${res.body}");
+  }
 }
 Future<void> ShowallEXpenseToday() async {
 
